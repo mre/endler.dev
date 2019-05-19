a90d8b313ccb830644eed1306e0b9b19ff27eb79
Matthias Endler <matthias-endler@gmx.net>
2019-05-19 22:21:01 +0200

    Add missing assets

---
+++ static/video.js
@@ -0,0 +1,18 @@
+// Adapted from Light YouTube Embeds by @labnol
+//  http://labnol.org/?p=27941
+
+function lightEmbedInit() {
+    var n, v = document.getElementsByClassName('light-video-embed');
+
+    for (n = 0; n < v.length; n++) {
+        v[n].onclick = function () {
+            var id = this.dataset.id;
+            var iframe = document.createElement('iframe');
+            var embed = 'https://www.youtube.com/embed/ID?autoplay=1';
+            iframe.setAttribute('src', embed.replace('ID', id));
+            iframe.setAttribute('frameborder', '0');
+            iframe.setAttribute('allowfullscreen', '1');
+            this.innerHTML = iframe.outerHTML;
+        };
+    }
+}
