fda235130679514092bb446868a7d403565b6d39
Matthias Endler <matthias-endler@gmx.net>
2019-12-03 21:55:49 +0100

    Formatting

--- static/video.js
+++ static/video.js
@@ -2,17 +2,18 @@
 //  http://labnol.org/?p=27941
 
 function lightEmbedInit() {
-    var n, v = document.getElementsByClassName('light-video-embed');
+  var n,
+    v = document.getElementsByClassName("light-video-embed");
 
-    for (n = 0; n < v.length; n++) {
-        v[n].onclick = function () {
-            var id = this.dataset.id;
-            var iframe = document.createElement('iframe');
-            var embed = 'https://www.youtube.com/embed/ID?autoplay=1';
-            iframe.setAttribute('src', embed.replace('ID', id));
-            iframe.setAttribute('frameborder', '0');
-            iframe.setAttribute('allowfullscreen', '1');
-            this.innerHTML = iframe.outerHTML;
-        };
-    }
+  for (n = 0; n < v.length; n++) {
+    v[n].onclick = function() {
+      var id = this.dataset.id;
+      var iframe = document.createElement("iframe");
+      var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+      iframe.setAttribute("src", embed.replace("ID", id));
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute("allowfullscreen", "1");
+      this.innerHTML = iframe.outerHTML;
+    };
+  }
 }
