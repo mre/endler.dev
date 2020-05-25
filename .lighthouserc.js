2609607de8f0344e69233d90b9a5e627398c933d
Matthias Endler <matthias-endler@gmx.net>
2020-05-25 12:49:00 +0200

    update

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,7 +1,12 @@
 module.exports = {
   ci: {
     upload: {
-      target: 'temporary-public-storage',
+      target: "lhci",
+      token: "b0ca1c00-afed-4b78-bee7-672c210499d3",
+      serverBaseUrl: "http://f1ac536d.ngrok.io",
+      // githubToken
+      // basicAuth.username
+      // basicAuth.password
     },
   },
 };
